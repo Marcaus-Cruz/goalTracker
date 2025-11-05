134652ec81c2b9d122bef11ecc4e8e56d2d5ffc1
Marcaus.Cruz <marcauscruz@gmail.com>
2025-11-05 12:06:27 -0800

    Add InfoBox to CourseGoalList

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,4 +1,6 @@
+import type { ReactNode } from "react";
 import CourseGoal from "./CourseGoal";
+import InfoBox from "./InfoBox";
 
 export type CourseGoalType = {
   id: number;
@@ -8,21 +10,47 @@ export type CourseGoalType = {
 
 export type CourseGoalListProps = {
   goals: CourseGoalType[];
-  onGoalDeleted: (id: number) => void; 
+  onGoalDeleted: (id: number) => void;
 };
 
-export default function CourseGoalList({ goals, onGoalDeleted }: CourseGoalListProps) {
+export default function CourseGoalList({
+  goals,
+  onGoalDeleted,
+}: CourseGoalListProps) {
+  if (!goals.length) {
+    return (
+      <InfoBox mode="hint">You have no goals yet. Start adding some!</InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning">
+        You have too many goals. Finish some first.
+      </InfoBox>
+    );
+  }
+
   return (
-    <form>
-      <ul>
-        {goals.map(({ id, title, description }) => (
-          <li key={id}>
-            <CourseGoal key={id} id={id} title={title} onDelete={onGoalDeleted}>
-              <p>{description}</p>
-            </CourseGoal>
-          </li>
-        ))}
-      </ul>
-    </form>
+    <>
+      {warningBox}
+      <form>
+        <ul>
+          {goals.map(({ id, title, description }) => (
+            <li key={id}>
+              <CourseGoal
+                key={id}
+                id={id}
+                title={title}
+                onDelete={onGoalDeleted}
+              >
+                <p>{description}</p>
+              </CourseGoal>
+            </li>
+          ))}
+        </ul>
+      </form>
+    </>
   );
 }
